429c6cc5fc30cbe1c9f33b69b264457ee296b976
YOUR_NAME <YOUR_EMAIL>
2025-04-17 14:05:22 -0400

    finshisdf

---
+++ amortizationCalculator.js
@@ -0,0 +1,53 @@
+let button1 = document.getElementById("button1");
+let output = document.getElementById("results");
+
+
+button1.addEventListener("click", function() {
+    let origLoan = parseInt(prompt("Enter loan amount"));
+    let downPayment = parseInt(prompt("Give the down payment"));
+    let loanTerm = parseInt(prompt("Give loan term (15 or 30)"));
+    if(loanTerm !== 15 && loanTerm !== 30) {
+        alert("MUST BE 15 OR 30");
+        return;
+    }
+
+    let downAmount = origLoan * (downPayment/100);
+    let principalAmount = origLoan - downAmount;
+    let rate = 0.0575/12;
+    let totalMonths = loanTerm * 12
+    let monthlyPayment = ((rate * principalAmount) / (1 - Math.pow(1 + rate, -totalMonths))).toFixed(2);
+    let monthlyNum = parseFloat(monthlyPayment)
+    let interestTotal = (monthlyNum * totalMonths) - principalAmount
+    let totalLoanCost = principalAmount + interestTotal;
+    output.textcontent = `Loan Term: ${loanTerm}`;
+    let rateP = document.createElement('p');
+    rateP.textContent = `Annual Interest rate: 5.75`;
+    let amountP = document.createElement('p');
+    amountP.textContent = `Principle Loan amount: ${principalAmount.toFixed(2)}`;
+    let interestP = document.createElement('p');
+    interestP.textContent = `Total Interest: ${interestTotal.toFixed(2)}`;
+    let totalCostP = document.createElement('p');
+    totalCostP.textContent = `Total Loan Cost: ${totalLoanCost.toFixed(2)}`;
+
+    output.appendChild(rateP);
+    output.appendChild(amountP);
+    output.appendChild(interestP);
+    output.appendChild(totalCostP);
+
+    let remaining = principalAmount;
+    for(let month = 1; month <= totalMonths; month++) {
+        let interestPaid = remaining * rate;
+        let PrinciplePaid = monthlyPayment - interestPaid
+        remaining -= PrinciplePaid
+
+        let table = document.createElement('p');
+        table.textContent = `Month: ${month} Interest: $${interestPaid.tofixed(2)} Principle = $${PrinciplePaid.tofixed(2)}`;
+        output.appendChild(table);
+        if(remaining <= 0) break;
+    }
+
+    let done = document.createElement('p');
+    done.textContent = "This ends the Amortization Calculator..."
+    output.appendChild(done);
+});
+
